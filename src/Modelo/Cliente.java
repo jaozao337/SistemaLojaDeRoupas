51d23e8c24223362b2c96c4a306d4ec457717075
jaozao337 <jsolagna@yahoo.com.br>
2020-07-17 20:03:22 -0300

    oi

--- src/Modelo/Cliente.java
+++ src/Modelo/Cliente.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Modelo;
 
-/**
- * GOSTOSO
- * @author ramom
- */
 public class Cliente {
-    
+// MEU PINTAO NO CU DO VBARBAS    
     
 }
